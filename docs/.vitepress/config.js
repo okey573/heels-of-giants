51557877a0d95545163b131492cd53611047b9c2
okey573 <854647481@qq.com>
2022-11-01 14:10:59 +0800

    init repo

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,110 @@
+module.exports = {
+  title: '复兴之路',
+  description: '一个前端的复兴之路',
+  base: '/',
+  head: [
+    ['link', { rel: 'icon', href: '/public/favicon.ico' }]
+  ],
+  ignoreDeadLinks: true,
+  lastUpdated: true,
+  themeConfig: {
+    logo: '/favicon.ico',
+    lastUpdatedText: '最近更新时间',
+    nav: nav(),
+    sidebar: sidebar()
+  }
+}
+
+function nav () {
+  return [
+    { text: '百度一下', link: 'https://baidu.com' },
+    {
+      text: '相关链接',
+      items: [
+        { text: 'vue.js', link: 'https://cn.vuejs.org/' },
+        { text: 'vitepress', link: 'https://vitepress.vuejs.org/' }
+      ]
+    }
+  ]
+}
+
+function sidebar () {
+  return [
+    {
+      text: 'JavaScript 基础',
+      collapsible: true,
+      items: [
+        { text: '原型链', link: '/JavaScript/原型链' },
+      ]
+    },
+    {
+      text: 'CSS 基础',
+      collapsible: true,
+      items: [
+        { text: '回流和重绘', link: '/CSS/回流和重绘' },
+      ]
+    },
+    {
+      text: 'VUE',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: '前端工程化',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: '性能优化',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: 'TypeScript',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: '网络',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: '设计模式',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: '算法',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: '安全',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: 'NodeJs',
+      collapsible: true,
+      items: [
+      ]
+    },
+    {
+      text: 'test',
+      collapsible: true,
+      items: [
+        { text: 'Test', link: '/Test/' },
+        { text: 'Guide', link: '/Test/guide' }
+      ]
+    }
+  ]
+}
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+layout: home
+
+hero:
+  name: Road to Revive
+  text: 一个前端的复兴之路
+  tagline: Let's do better...
+
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /Test/guide
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com
+
+features:
+  - icon: ⚡️
+    title: Complete
+    details: ...
+  - icon: 🖖
+    title: Exactly
+    details: ...
+---
