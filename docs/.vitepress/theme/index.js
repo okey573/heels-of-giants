aa64e68ead065ff9f9a4aaaa7bfde65442a7fd28
okey573 <854647481@qq.com>
2022-11-02 10:05:26 +0800

    添加element-plus组件库

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,11 @@
+import DefaultTheme from 'vitepress/theme'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
+export default {
+  ...DefaultTheme,
+  enhanceApp ({ app }) {
+    app.use(ElementPlus)
+  }
+}
