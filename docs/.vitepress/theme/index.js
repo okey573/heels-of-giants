3e9aac40ae221138f0b1b77a7ab66e099472468e
okey573 <854647481@qq.com>
2022-11-02 10:15:23 +0800

    暂时注释掉element-plus

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
 
 
 export default {
   ...DefaultTheme,
   enhanceApp ({ app }) {
-    app.use(ElementPlus)
+    // app.use(ElementPlus)
   }
 }
