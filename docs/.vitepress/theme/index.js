b179b2b3b05a96fbbd315caace94d32c6a460dc2
okey573 <854647481@qq.com>
2023-02-20 10:49:05 +0800

    移除ElementPlus

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
 import './styles/vars.css'
 
 
 export default {
   ...DefaultTheme,
   enhanceApp ({ app }) {
-    // app.use(ElementPlus)
   }
 }
