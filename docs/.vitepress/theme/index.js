042e7b8f95495650d632c2c4f990beaeca1f868b
okey573 <854647481@qq.com>
2023-02-28 13:55:41 +0800

    自定义鼠标

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
+import './styles/mouse.css'
 import './styles/vars.css'
 import NProgress from './plugins/NProgress'
 import ClickAnimation from './plugins/ClickAnimation'
