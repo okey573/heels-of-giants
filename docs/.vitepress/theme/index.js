05b2b4a73cc4ce51a83c442a9f777d716345b078
okey573 <854647481@qq.com>
2023-11-14 09:50:12 +0800

    去掉click动画

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,12 +2,12 @@ import DefaultTheme from 'vitepress/theme'
 import './styles/mouse.css'
 import './styles/vars.css'
 import NProgress from './plugins/NProgress'
-import ClickAnimation from './plugins/ClickAnimation'
+// import ClickAnimation from './plugins/ClickAnimation'
 
 export default {
   ...DefaultTheme,
   enhanceApp (ctx) {
     NProgress(ctx, { showSpinner: false })
-    ClickAnimation()
+    // ClickAnimation()
   }
 }
